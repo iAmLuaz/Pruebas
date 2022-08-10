73c9220d10f28a9df085abca1d0d82b596f77723
iAmLuaz <105666817+iAmLuaz@users.noreply.github.com>
2022-08-10 17:00:43 -0500

    Add files via upload

--- Prueba1/Prueba1/app.js
+++ Prueba1/Prueba1/app.js
@@ -1,44 +1,22 @@
-//Recibir valores de todos los datos
-let names = document.querySelectorAll('div.name');
-let membership = document.querySelectorAll('div.membership');
-let address = document.querySelectorAll('div.address');
-
-//Lugar donde se almacena el Array de Datos
-let nameArray = [];
-
 //Funcion para quitar acentos
 const removeAccents = (param) => {
     return param.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-
 }
-//Obtener la cadena completa de nombres, convertirlos a mayuscula y quitarle acentos
-for (let i = 0; i < names.length; i++) {
-    var currentName = names[i].innerHTML;
-    nameArray.push(removeAccents(currentName).toUpperCase());
-}
-console.log(nameArray);
-
-//Funcion para recibir los datos ingresados y transformarlos en mayusculas y quitarle acentos
-document.getElementById('searcher').onkeydown = transformName;
-document.getElementById('searcher').onkeyup = transformName;
-function transformName(){
-    let form = document.forms['form'];
-    let output = '';
-    let name = form['searcher'];
-    output = removeAccents(name.value).toUpperCase();
-    document.getElementById('elementos').innerHTML = output;
-    console.log('Working');
-}
-
-
-
-
-// function searchName() {
-//     let receivedData = document.getElementById('elementos').innerHTML;
-//     if (receivedData = ) {
+//Funcion para recibir los datos y filtrarlos
+document.addEventListener('input', e =>{
+    if (e.target.matches('.searcher')) {
+        document.querySelectorAll('.name').forEach(toggleClass => {
+            removeAccents(toggleClass.textContent).toUpperCase().includes(removeAccents(e.target.value).toUpperCase())
+            ?toggleClass.parentElement.classList.remove('boxhidden')
+            :toggleClass.parentElement.classList.add('boxhidden')
+        })
+    }
+})
+document.getElementById('filtermember').onclick = filterGold;
+function filterGold() {
+    document.getElementsByClassName('membership')
+    if (condition) {
         
-//     } else {
-        
-//     }
-//     console.log('Working too');
-// }
+    }
+    console.log('working')
+}
